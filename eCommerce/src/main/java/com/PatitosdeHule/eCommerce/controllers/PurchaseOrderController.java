5481a4ce51915776e357315613e23d9c8b825b02
Nico Sandrin <94770291+Sheckerd@users.noreply.github.com>
2022-06-27 15:23:31 -0300

    Delete eCommerce directory

--- eCommerce/src/main/java/com/PatitosdeHule/eCommerce/controllers/PurchaseOrderController.java
+++
@@ -1,7 +0,0 @@
-package com.PatitosdeHule.eCommerce.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PurchaseOrderController {
-}
--- eCommerce/src/main/java/com/PatitosdeHule/eCommerce/repositories/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.PatitosdeHule.eCommerce.repositories;
-
-import com.PatitosdeHule.eCommerce.models.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-
-}

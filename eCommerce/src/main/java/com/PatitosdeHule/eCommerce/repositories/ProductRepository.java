ff1112a87200ad90cc4bba2422ab657eb5896f5a
Sheckerd <nrsandrin@hotmail.com>
2022-06-27 16:05:42 -0300

    27/6

---
+++ eCommerce/src/main/java/com/PatitosdeHule/eCommerce/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.PatitosdeHule.eCommerce.repositories;
+
+
+import com.PatitosdeHule.eCommerce.models.Product;
+import com.PatitosdeHule.eCommerce.models.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
